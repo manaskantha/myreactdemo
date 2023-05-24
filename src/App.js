be4359592a1e85aead9942ac9b489a4a2f0f7ad8
ManasK <manaskantha@users.noreply.github.com>
2023-05-24 13:29:47 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./mycomponents/styles.css";
 import DisplayDate from "./mycomponents/DateHeader";
-import Store from "./reduxlib/Store";
 
 export default function App() {
   const newDate = new Date();
@@ -11,9 +10,7 @@ export default function App() {
         {/* <span>Please</span> <span>enter your </span>
         <span>details below</span> */}
         <DisplayDate date={newDate} />
-        
       </>
-      {/* <Store /> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App";
 import SayHello from "./mycomponents/SayHello";
 import Greeting from "./mycomponents/UserForm";
 import MainApp from "./mycomponents/UseEffect";
 import GetMovies from "./mycomponents/FetchMovies";
+import store from "./reduxlib/Store";
+import Counter from "./mycomponents/Counter";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -16,11 +19,14 @@ console.log(element);
 
 root.render(
   <StrictMode>
-    <App />
-    <br />
-    <SayHello />
-    <Greeting />
-    <br />
-    <GetMovies />
+    <Provider store={store}>
+      <App />
+      <br />
+      <SayHello />
+      <Greeting />
+      <br />
+      <GetMovies />
+      <Counter />
+    </Provider>
   </StrictMode>
 );
